f4c5bd5a4b3ecd2f5c8d37de30111cf8782ef91d
goodguytim1 <timobrien56@knights.ucf.edu>
2021-03-02 08:17:14 -0500

    Create Basic Item System

--- exercise/amazon-cart/src/components/CartItems.js
+++ exercise/amazon-cart/src/components/CartItems.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import CartItem from './CartItem.js'
 import './CartItems.css'
 function CartItems() {
     return (
         <div className="CartItems">
           <h1>Shopping Cart</h1>
+          <hr/>
+          <div className="CartItems-items">
+            <CartItem/>
+          </div>
         </div>
     )
 }
